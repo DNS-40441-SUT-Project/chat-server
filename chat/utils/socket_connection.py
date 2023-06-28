cc0e4083efbee39a1dabb7001e52794fcf01c591
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-06-28 23:38:14 +0330

    fix: fix ServerSocketConnection children

--- chat/utils/socket_connection.py
+++ chat/utils/socket_connection.py
@@ -4,10 +4,12 @@ from django.conf import settings
 
 class ServerNormalSocketConnection(ServerSocketConnection):
     _listen_port = settings.SERVER_PORT
+    _limit = 10000
 
 
 class ServerPollConnection(ServerSocketConnection):
     _listen_port = settings.POLL_PORT
+    _limit = 10000
 
 
 def accept_connection():
