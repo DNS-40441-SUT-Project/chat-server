f98d7a2a54419fa484cbdd70a862e074e4237cb1
Ashk Khademian <ashkan.khd.q@gmail.com>
2023-06-30 01:08:30 +0330

    fix: fix session creation

--- chat/data/poll_connection.py
+++ chat/data/poll_connection.py
@@ -1,23 +1,25 @@
-from typing import Dict
+import dataclasses
+from typing import Dict, Tuple
 
 from chat.utils.socket_connection import ServerPollConnection
 
 
+@dataclasses.dataclass
 class LoginUserContext:
     poll_connection: ServerPollConnection
     symmetric_key: str
 
 
 class PollConnections:
-    _all_poll_connections: Dict[str, ServerPollConnection] = {}
+    _all_poll_connections: Dict[str, LoginUserContext] = {}
 
     @classmethod
-    def get(cls, username: str):
+    def get(cls, username: str) -> LoginUserContext:
         return cls._all_poll_connections.get(username)
 
     @classmethod
-    def add(cls, username: str, conn: ServerPollConnection):
-        cls._all_poll_connections[username] = conn
+    def add(cls, username: str, context: LoginUserContext):
+        cls._all_poll_connections[username] = context
 
     @classmethod
     def remove(cls, username: str):
