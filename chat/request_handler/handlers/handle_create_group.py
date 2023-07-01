bb819cfd3776c4fe622a2075d8a810573de966d8
Ehsan200 <ehsan.movaffagh@icloud.com>
2023-07-01 22:23:53 +0330

    feat: Add create group

---
+++ chat/request_handler/handlers/handle_create_group.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from connection_utils.socket_message import SocketMessage
+
+from chat.data import PollConnections
+from chat.db_controller import authenticated_user, create_group
+from chat.utils.signature import sign_data
+from chat.utils.socket_connection import ServerNormalSocketConnection
+
+
+def handle_create_group(conn: ServerNormalSocketConnection, message: SocketMessage):
+    user = authenticated_user(**message.headers['authentication'])
+    signature_value = '200'
+    create_group(admin_username=user.username, group_name=message.body['group_name'])
+    conn.send_sym_encrypted(
+        path='group created successfully',
+        data=dict(
+            status=signature_value,
+        ),
+        headers=dict(
+            signature_value=signature_value,
+            signature=sign_data(signature_value),
+            T=datetime.now().timestamp(),
+        ),
+        symmetric_key=PollConnections.get(user.username).encoded_symmetric_key
+    )
